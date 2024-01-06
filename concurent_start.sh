66aa25d2fe9a0e07d7c5552f66a4f63a6b8d503e
Ivan Shputov <blackjacker@Ivans-MacBook-Pro-2.local>
2024-01-06 13:59:28 +0100

    upd

--- concurent_start.sh
+++ concurent_start.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 python task_server.py & 
-python task_manager.py --public_ip "$1" &
-python app.py &
+python task_manager.py --public_ip "$1" --task_manager_port "$2"&
+python app.py --port "$3" &
 
 # Prevent the script from exiting
 wait
