606e6e6eee12ea86e2db43b20b6e3bac7f5f8fba
Ivan Shputov <blackjacker@Ivans-MacBook-Pro-2.local>
2024-03-06 21:08:06 +0100

    debugg methods

--- task_server.py
+++ task_server.py
@@ -24,7 +24,7 @@ parser.add_argument('--database_name', default='app', help='MongoDB database nam
 parser.add_argument('--flask_port', type=int, default=8100, help='Port for the Flask server.')
 parser.add_argument('--flask_host', default='0.0.0.0', help='Host for the Flask server.')
 parser.add_argument('--blob_storage_path', type=str, default='files/blob_storage', help='Path to the blob storage directory.')
-parser.add_argument('--template_config_path', type=str, default='files/styles_config.csv', help='Path to the template config')
+parser.add_argument('--template_config_path', type=str, default='files/styles_config.json', help='Path to the template config')
 parser.add_argument('--styles_config_storage', type=str, default='files/configs', help='Path to the blob storage directory.')
 args = parser.parse_args()
 
@@ -48,8 +48,13 @@ def require_valid_uuid(task_db):
     def decorator(view_function):
         @wraps(view_function)
         def decorated_function(*args, **kwargs):
-            data = request.get_json(force=True)
-            token = data.get('token', None)
+            data = request.get_json(silent=True)
+            if data is not None:
+                token = data.get('token', None)
+            else:
+                data = request.form['data']
+                data = json.loads(data)
+                token = data.get('token', None)
             if token and task_db.uuid_exists(token):
                 return view_function(*args, **kwargs)
             else:
@@ -60,8 +65,11 @@ def require_valid_uuid(task_db):
 
 @app.route('/register_for_token', methods=['POST'])
 def register_for_token():
-    data = request.get_json(force=True)
-    token = data.get('token', None)
+    data = request.get_json(silent=True)
+    if data:
+        token = data.get('token', None)
+    else:
+        token = None
     if not task_db.check_uuid_exists(token):
         token = str(uuid.uuid4())
         task_db.store_link_with_uuid(token)
@@ -81,38 +89,15 @@ def store_apns():
     
     is_ok = task_db.update_collection_with_prop(token, apns_token, 'apns_token')
     if is_ok:
-        return jsonify({'token': token}), 200
+        return jsonify({'status': f"ok, store apns {apns_token} for {token} token"}), 200
     else:
-        return jsonify({'error': 'something wrong, zero object was modified'}), 400
+        return jsonify({'status': 'error, something wrong, zero object was modified'}), 400
 
 
 @app.route('/get_templates', methods=['GET'])
 @require_valid_uuid(task_db)
 def get_templates():
-    data = {'categories': {}}
-
-    with open(args.template_config_path, mode='r') as csv_file:
-        csv_reader = csv.DictReader(csv_file)
-        for row in csv_reader:
-            category = row['type']
-            if category not in data['categories']:
-                data['categories'][category] = {
-                    'name': category.capitalize() + ' styles',
-                    'templates': []
-                }
-
-            image_url = url_for('get_image', filename=row['image_path'], _external=True)
-            
-            data['categories'][category]['templates'].append({
-                'id': row['id'],
-                'title': row['title'],
-                'image_url': image_url, 
-                'description': row['description'],
-            })
-
-    # Convert the categories dictionary to a list
-    data['categories'] = list(data['categories'].values())
-
+    data = json.load(open(args.template_config_path, mode='r'))
     return jsonify(data)
 
 
@@ -143,16 +128,18 @@ def get_task_result(task_id):
 @require_valid_uuid(task_db)
 @limiter.limit("10 per minute") 
 def upload_task():
-    # Check if a video file is present in the request
+    print(request.files,flush=True)
     if 'video' not in request.files:
         return jsonify({'error': 'No video part in the request'}), 400
     file = request.files['video']
-    
+
+    print('file', file)
     # If the user does not select a file, the browser submits an empty file without a filename
-    if file.filename == '':
+    if 'data' not in request.form:
         return jsonify({'error': 'No selected file'}), 400
     
-    data = request.get_json(force=True)
+    data = request.form['data']
+    data = json.loads(data)
     
     token = data.get('token')
     objects = data.get('objects')
@@ -169,12 +156,12 @@ def upload_task():
 
     with open(storage_json_path, 'w') as file:
         json.dump(
-            json.loads(config_text_box), file, indent=4
+            config_text_box, file, indent=4
         )
 
     # Insert task into the MongoDB database
     task_id = task_db.insert_task(
-        objects=json.loads(objects),
+        objects=objects,
         original_video_path=storage_video_path,
         config_path=storage_json_path,
         task_type=task_type,
@@ -189,10 +176,14 @@ def upload_task():
 
 
 @app.route('/process_video_result', methods=['POST'])
+@require_valid_uuid(task_db)
 @limiter.limit("10 per minute")
 def process_video_result():
-    task_id = request.form['task_id']
+    data = request.form['data']
+    data = json.loads(data)
+    task_id = data['task_id']
     video_file = request.files['video']
+
     task_dir = Path(args.blob_storage_path) / task_id
     task_dir.mkdir(exist_ok=True)
     video_path = os.path.join(str(task_dir), task_id + '_' + video_file.filename)
@@ -201,13 +192,14 @@ def process_video_result():
     video_file.save(video_path)
 
     # Update task in MongoDB
-    task_db.move_task_to_done(task_id, video_path)
+    # task_db.move_task_to_done(task_id, video_path)
 
     user_id = task_db.get_user_id_by_task_id(task_id)
 
     apns_token = task_db.get_user_db_property(user_id, 'apns_token')
 
-    send_notification_to_popup(apns_token, f'process video result for task {task_id}')
+    print(f'send_notification_to_popup with {apns_token}')
+    # send_notification_to_popup(apns_token, f'process video result for task {task_id}')
 
     return jsonify({'message': 'Video processed and task updated successfully'}), 200
 
